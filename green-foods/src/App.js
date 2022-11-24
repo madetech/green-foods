3ee96976e86e286cb01cf5f08eb48f313307fe12
AprilMD <95350189+AprilMD@users.noreply.github.com>
2022-11-24 12:23:59 +0000

    add carbon per 100g for product on barcode search

--- green-foods/src/App.js
+++ green-foods/src/App.js
@@ -8,12 +8,14 @@ function App() {
   const [userInput, setUserInput] = useState(" ");
   const [productName, setProductName] = useState(" ");
   const [imageURL, setImageURL] = useState(" ")
+  const [carbonTotal, setCarbonTotal] = useState(0)
 
   const makecalltoAPI = (userInput) => {
     axios(`https://world.openfoodfacts.org/api/v0/product/${userInput}.json`)
     .then(response => {
       setProductName(response.data.product.brands_tags[0])
       setImageURL(response.data.product.image_front_small_url)
+      setCarbonTotal(response.data.product.ecoscore_data.agribalyse.co2_total)
     })
     .catch(error => {
     console.log(error)
@@ -45,6 +47,9 @@ function App() {
         productName here: {productName}
       </p>
       <img src={imageURL}></img>
+      <h1>
+        {Math.round(carbonTotal*100)}g CO² per 100g of product
+      </h1>
     </div>
   );
 }
