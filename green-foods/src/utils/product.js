a543685bda30abd76221e761ade2f54bbc2538f2
Lara Santos <lara.santos@madetech.com>
2022-11-28 16:24:54 +0000

    extract util functions and ProductWithBarcode

---
+++ green-foods/src/utils/product.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const getProductWithBarcode = async (userInput) => {
+    const response = await axios(`https://world.openfoodfacts.org/api/v0/product/${userInput}.json`)
+    const prod = response.data.product
+
+    const productInfo = {
+      name: prod.product_name,
+      image: prod.image_front_small_url,
+      carbon: prod.ecoscore_data.agribalyse?.co2_total
+    }
+    return productInfo
+}
+
+export {getProductWithBarcode}
