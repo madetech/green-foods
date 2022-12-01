35dbe08fe84551d4adc628fed24008da897fb83f
asha burathoki <aburathoki@gmail.com>
2022-12-01 16:43:34 +0000

    Add unit test for invalid barcode

--- green-foods/src/utils/product.js
+++ green-foods/src/utils/product.js
@@ -1,15 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getProductWithBarcode = async (userInput) => {
-    const response = await axios(`https://world.openfoodfacts.org/api/v0/product/${userInput}.json`)
-    const prod = response.data.product
+  const response = await axios(
+    `https://world.openfoodfacts.org/api/v0/product/${userInput}.json`
+  );
+  console.log(response);
+  const prod = response.data.product;
 
+  if (prod) {
     const productInfo = {
       name: prod.product_name,
       image: prod.image_front_small_url,
-      carbon: prod.ecoscore_data.agribalyse?.co2_total || null
-    }
-    return productInfo
-}
+      carbon: prod.ecoscore_data.agribalyse?.co2_total || null,
+    };
+    return productInfo;
+  }
+  const productInfo = {
+    name: null,
+    image: null,
+    carbon: null,
+    message: response.data.status_verbose,
+  };
+  return productInfo;
+};
 
-export {getProductWithBarcode}
+export { getProductWithBarcode };
--- green-foods/src/utils/product.test.js
+++ green-foods/src/utils/product.test.js
@@ -7,7 +7,6 @@ jest.mock("axios");
 // check if JSON response returns correct
 describe("searchProductWithBarcode", () => {
   it("returns a productInfo object", async () => {
-
     axios.mockResolvedValueOnce({
       data: {
         product: {
@@ -24,13 +23,12 @@ describe("searchProductWithBarcode", () => {
 
     const product = await getProductWithBarcode("5053990138722");
 
-    expect(product.name).toBe("Pringles Original")
-    expect(product.image).toBe("url")
-    expect(product.carbon).toBe("1.87")
+    expect(product.name).toBe("Pringles Original");
+    expect(product.image).toBe("url");
+    expect(product.carbon).toBe("1.87");
   });
 
   it("returns null-value for carbon when data is not present", async () => {
-
     axios.mockResolvedValueOnce({
       data: {
         product: {
@@ -43,9 +41,26 @@ describe("searchProductWithBarcode", () => {
 
     const product = await getProductWithBarcode("5053990138722");
 
-    expect(product.name).toBe("Pringles Original")
-    expect(product.image).toBe("url")
-    expect(product.carbon).toBe(null)
+    expect(product.name).toBe("Pringles Original");
+    expect(product.image).toBe("url");
+    expect(product.carbon).toBe(null);
+  });
+
+  it("returns null values when given invalid barcode", async () => {
+    axios.mockResolvedValueOnce({
+      data: {
+        code: null,
+        status: 0,
+        status_verbose: "no code or invalid code",
+      },
+    });
+
+    const product = await getProductWithBarcode("not a barcode");
+
+    expect(product.name).toBe(null);
+    expect(product.image).toBe(null);
+    expect(product.carbon).toBe(null);
+    expect(product.message).toBe("no code or invalid code");
   });
 });
 // check if not 200 response status
