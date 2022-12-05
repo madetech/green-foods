ab845ea75268bd700434bc3010ad5eee850b4a16
James Coker <1jamescoker1@gmail.com>
2022-12-05 11:03:05 +0000

    create util function to get products by name and test

---
+++ green-foods/src/components/searchProductByName.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import React from 'react';
+
+const SearchProductByName = () => {
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SearchProductByName;
