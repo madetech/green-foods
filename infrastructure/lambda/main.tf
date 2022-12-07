794a8734238e096a7343aa8c02fff80c4733fad4
James Coker <1jamescoker1@gmail.com>
2022-12-07 12:34:27 +0000

    add getProductByBarcode lambda function

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -28,3 +28,34 @@ resource "aws_s3_object" "lambda" {
    source = data.archive_file.lambda.output_path
    etag = filemd5(data.archive_file.lambda.output_path)
 }
+
+resource "aws_iam_role" "lambda" {
+  name = "${var.environment}-green-foods-getProductByBarcode-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda" {
+  role = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "get_product_by_barcode" {
+  function_name = "${var.environment}-green-foods-getProductByBarcode"
+  s3_bucket = aws_s3_bucket.lambda.id
+  s3_key = aws_s3_object.lambda.key
+  runtime = "nodejs18.x"
+  handler = "handler.run"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+  role = aws_iam_role.lambda.arn
+}
