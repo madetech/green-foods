e5e935da74dcbe2251cc758e889d3e18de841262
Ian King <ian.antking@gmail.com>
2022-12-08 17:02:50 +0000

    include/exclude tests in tsconfig, add lintin

--- services/getProductByBarcode/handler.ts
+++ services/getProductByBarcode/handler.ts
@@ -1,17 +1,17 @@
-import {APIGatewayProxyResult, APIGatewayProxyEventV2} from "aws-lambda"
-import axios from "axios";
+import { APIGatewayProxyResult, APIGatewayProxyEventV2 } from 'aws-lambda'
+import axios from 'axios'
 
 export const run = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResult> => {
-    const response = await axios(
+  const response = await axios(
         `https://world.openfoodfacts.org/api/v0/product/${event.pathParameters?.barcode}.json`
-      );
-      const product = response.data.product;
+  )
+  const product = response.data.product
 
-      const productInfo = {
-        name: product.product_name,
-        image: product.image_front_small_url,
-        carbon: product.ecoscore_data.agribalyse?.co2_total,
-      }
+  const productInfo = {
+    name: product.product_name,
+    image: product.image_front_small_url,
+    carbon: product.ecoscore_data.agribalyse?.co2_total
+  }
 
-    return {statusCode: 200, body: JSON.stringify(productInfo)}
+  return { statusCode: 200, body: JSON.stringify(productInfo) }
 }
