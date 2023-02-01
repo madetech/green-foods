9e903eaff7804cd735aac3e49153e6c9c7a9ae50
Josephine Obeng <64155953+Joseaie@users.noreply.github.com>
2023-02-01 13:04:25 +0000

    Style ProductCard

--- green-foods/src/components/ProductCard/ProductCard.js
+++ green-foods/src/components/ProductCard/ProductCard.js
@@ -1,13 +1,14 @@
 import H1 from '../typography/H1/H1';
 import P from '../typography/P/P';
+import './ProductCard.css';
 
 const ProductCard = ({ carbonTotal, imgSrc, productName }) => {
   return (
-    <>
+    <div className='card'>
       <img src={imgSrc} alt={productName} />
       <H1 title={productName} />
       <P text={`${Math.round(carbonTotal * 100)}g CO² per 100g of product`} />
-    </>
+    </div>
   );
 };
 
